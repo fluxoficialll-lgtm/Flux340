734b2a3a9de2ad8bf89e4082a5c0b547b5980890
deutscher3036 <deutscher3036@gmail.com>
2026-02-12 03:12:29 +0000

    fix: Corrige redirecionamento incorreto nas rotas de configuração

--- routes/modules/SettingsRoutes.tsx
+++ routes/modules/SettingsRoutes.tsx
@@ -1,14 +1,12 @@
+
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { FinancialPanel } from '../../pages/FinancialPanel';
 import { ProviderConfig } from '../../pages/ProviderConfig';
-import TransactionHistoryPage from '../../pages/TransactionHistoryPage'; // Importa a nova página
+import TransactionHistoryPage from '../../pages/TransactionHistoryPage';
 
-export const SettingsRoutes = (
-  <>
-    <Route path="/financial" element={<FinancialPanel />} />
-    <Route path="/financial/providers" element={<ProviderConfig />} />
-    {/* Adiciona a nova rota para o histórico de transações */}
-    <Route path="/financial/transactions" element={<TransactionHistoryPage />} />
-  </>
-);
+export const SettingsRoutes = [
+    <Route key="financial" path="/financial" element={<FinancialPanel />} />,
+    <Route key="financial-providers" path="/financial/providers" element={<ProviderConfig />} />,
+    <Route key="financial-transactions" path="/financial/transactions" element={<TransactionHistoryPage />} />
+];
