1c042ae95c7be0b310bb3ba5da34f820f4a957a2
deutscher3036 <deutscher3036@gmail.com>
2026-02-12 02:58:04 +0000

    fix: Corrige caminho de importação em TransactionHistoryPage

--- pages/TransactionHistoryPage.tsx
+++ pages/TransactionHistoryPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import CashFlowChart from '../../components/financial/CashFlowChart';
-import TransactionHistory from '../../components/financial/TransactionHistory';
+import CashFlowChart from '../components/financial/CashFlowChart';
+import TransactionHistory from '../components/financial/TransactionHistory';
 
 const TransactionHistoryPage: React.FC = () => {
     return (
