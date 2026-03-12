0ee7c11636ffd24d03a7cc0b3d6170fe57024f82
nagasakitakit <nagasakitakit@gmail.com>
2026-03-12 16:32:30 +0000

    Apply fixes and updates

--- backend/RotasBackend/Rotas.Criacao.Perfil.Flux.js
+++ backend/RotasBackend/Rotas.Criacao.Perfil.Flux.js
@@ -1,13 +1,13 @@
 
 import express from 'express';
-import controle from '../../controles/Controles.Criacao.Perfil.Flux.js';
-import { verificarToken } from '../../config/Middleware.Autenticacao.JWT.js';
+import controle from '../controles/Controles.Criacao.Perfil.Flux.js';
+import authMiddleware from '../config/Middleware.Autenticacao.JWT.js';
 
 const router = express.Router();
 
-router.get('/', verificarToken, controle.buscarPerfil);
-router.put('/', verificarToken, controle.atualizarPerfil);
-router.delete('/', verificarToken, controle.deletarPerfil);
+router.get('/', authMiddleware, controle.buscarPerfil);
+router.put('/', authMiddleware, controle.atualizarPerfil);
+router.delete('/', authMiddleware, controle.deletarPerfil);
 router.get('/:userId', controle.buscarPerfilPublico);
 
 export default router;
