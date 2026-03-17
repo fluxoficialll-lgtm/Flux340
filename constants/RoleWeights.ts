144a27a6e5531a2c36c66bd014a49499137816a8
nagasakitakit <nagasakitakit@gmail.com>
2026-03-17 21:23:16 +0000

    Atualizações do projeto

--- constants/RoleWeights.ts
+++
@@ -1,9 +0,0 @@
-
-export const ROLE_WEIGHTS: Record<string, number> = {
-    'Dono': 100,
-    'Admin': 70,
-    'Moderador': 50,
-    'Membro': 10
-};
-
-export type RoleName = keyof typeof ROLE_WEIGHTS;
